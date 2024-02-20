93c174f0c873478e45cfc1ee1c70792ea6f08a84
shikahJS <shikah.sunaidi@gmail.com>
2024-02-20 03:51:53 +0300

    Add ToolReports and update evaluator script

--- Scripts/evaluator.py
+++ Scripts/evaluator.py
@@ -1,18 +1,44 @@
 import pandas as pd
 from sklearn import metrics
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
 
 def eval(tool):
     ToolDS = pd.read_csv('./Results/LabeledData/'+tool+'.csv')
-    BaseDS = pd.read_csv('./Benchmarks/EDA_Outcomes/mult-label_CGT_DS.csv')
+    BaseDS = pd.read_csv('./Benchmarks/EDA_Outcomes/cgt_MultiDS_StudySet.csv')
 
     predicted = createDASPmetrics(tool,ToolDS)
-    BaseDS.drop(BaseDS[~BaseDS['fp_sol'].isin(ToolDS['contractAddress'])].index, inplace=True)
-    BaseDS.reset_index(inplace=True, drop=True)        
     actual = createDASPmetrics('Base',BaseDS)
-    #actual = actual.drop_duplicates(keep='last')
-    
-    confusionMatrix = metrics.confusion_matrix(actual, predicted)
-    print(confusionMatrix)
+
+    while len(predicted['id']) != len(actual['id']):
+        if len(predicted['id']) > len(actual['id']):
+            predicted.drop(predicted[~predicted['id'].isin(actual['id'])].index, inplace=True)    
+            predicted.reset_index(inplace=True, drop=True)
+        else:
+            actual.drop(actual[~actual['id'].isin(predicted['id'])].index, inplace=True)    
+            actual.reset_index(inplace=True, drop=True)
+
+    predicted.drop('id', axis=1, inplace=True)
+    actual.drop('id',axis=1, inplace=True)
+
+    print('Predicted Shape: ', predicted.shape)
+    print('Actual Shape: ', actual.shape)
+
+    predicted = predicted.values.tolist()
+    actual = actual.values.tolist()
+
+    confusionMatrix = metrics.multilabel_confusion_matrix(actual, predicted)
+    plot_confusionMatrix(confusionMatrix, tool)
+
+    from sklearn.metrics import classification_report, confusion_matrix
+
+    print(classification_report(actual, predicted,zero_division=0))
+
+    '''from sklearn.metrics import precision_recall_fscore_support as score
+    precision,recall,fscore,support=score(actual,predicted,average='weighted')
+    print ('Precision : {}'.format(precision))
+    print ('Recall    : {}'.format(recall))
+    print ('F-score   : {}'.format(fscore))'''
 
 def createDASPmetrics(tool,DS):
     DASPmetrics =  pd.DataFrame(columns=['id','1','2','3','4','5','6','7','8','9'])
@@ -20,7 +46,7 @@ def createDASPmetrics(tool,DS):
     DASP_Label = ''
 
     if tool == 'Base':
-        address = 'addr'
+        address = 'fp_sol'
         DASP_Label = 'DASP'
     else:
         address = 'contractAddress'
@@ -34,6 +60,26 @@ def createDASPmetrics(tool,DS):
         else:
             for i in range(1,10):
                 DASPmetrics.at[index, str(i)] = 1 if str(i) in DS[DASP_Label].iloc[index] else 0
+    
+    DASPmetrics.sort_values('id',inplace=True)
+    DASPmetrics.reset_index(inplace=True, drop=True)
     return DASPmetrics
 
-eval('Solhint')
+def plot_confusionMatrix(confusionMatrix,tool):
+    DASP_Labels = ['Reentrancy','Access Control','Arithmetic','Unchecked Return Values','DoS','Bad Randomness','Front-Running','Time manipulation','Short Address Attack']
+    
+    n_plots=len(confusionMatrix)
+    fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(25,15))
+
+    for i, ax in enumerate(axes.flatten()):
+        if i < n_plots :
+            confusionMatrix_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusionMatrix[i], display_labels = [True,False])
+            confusionMatrix_display.plot(ax=ax,cmap=plt.cm.Blues)
+            confusionMatrix_display.ax_.set_title(DASP_Labels[i],size=10)
+            confusionMatrix_display.im_.colorbar.remove()
+    fig.suptitle("Confusion Matrices of " + tool + ' Tool', size=16, y=0.93)
+    plt.subplots_adjust(wspace=0.10, hspace=0.5)
+    fig.colorbar(confusionMatrix_display.im_, ax=axes)
+    plt.show()
+
+#eval('Solhint')
